63034cfb9a43cf36de38fe8da1689bdfd8a71027
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-04-18 16:40:41 -0500

    created and tested the routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 // This file will import all of the API routes to prefix their endpoint names and package them up.
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+
+router.use('/comments', commentRoutes);
 
 module.exports = router;
